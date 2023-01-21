6fb0eec3509f6ab07e1776b689629fc006ebfde5
Millzy <millzyyt08@gmail.com>
2023-01-21 11:36:10 +0000

    UI values

--- include/UI/ViewControllers/SettingsViewController.hpp
+++ include/UI/ViewControllers/SettingsViewController.hpp
@@ -1,14 +1,47 @@
 #pragma once
 
 #include "custom-types/shared/macros.hpp"
+#include "bsml/shared/macros.hpp"
 
 #include "HMUI/ViewController.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+#include "UnityEngine/UI/HorizontalLayoutGroup.hpp"
+
+#define TextMeshProUGUI TMPro::TextMeshProUGUI
+#define HorizontalLayoutGroup UnityEngine::UI::HorizontalLayoutGroup
 
 #define GET_METHOD(method) il2cpp_utils::il2cpp_type_check::MetadataGetter<&method>::get()
 
 DECLARE_CLASS_CODEGEN(RumbleMod::UI::ViewControllers, SettingsViewController,::HMUI::ViewController,
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, noteEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, noteInteractables);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, chainEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, chainInteractables);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, arcEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, arcInteractables);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, saberEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, saberInteractables);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, wallEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, wallInteractables);
+    DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, uiEnabledText);
+    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, uiInteractables);
+
     DECLARE_OVERRIDE_METHOD(void, DidActivate, GET_METHOD(::HMUI::ViewController::DidActivate),
                             bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
 
-    // Do BSML values here or smth
+    DECLARE_BSML_PROPERTY(bool, noteEnabled);
+    DECLARE_BSML_PROPERTY(float, noteStrength);
+    DECLARE_BSML_PROPERTY(bool, chainEnabled);
+    DECLARE_BSML_PROPERTY(float, chainStrength);
+    DECLARE_BSML_PROPERTY(bool, arcEnabled);
+    DECLARE_BSML_PROPERTY(float, arcStrength);
+    DECLARE_BSML_PROPERTY(bool, saberEnabled);
+    DECLARE_BSML_PROPERTY(float, saberStrength);
+    DECLARE_BSML_PROPERTY(bool, wallEnabled);
+    DECLARE_BSML_PROPERTY(float, wallStrength);
+    DECLARE_BSML_PROPERTY(bool, uiEnabled);
+    DECLARE_BSML_PROPERTY(float, uiStrength);
 )
+
+#undef TextMeshProUGUI
+#undef HorizontalLayoutGroup
--- src/UI/ViewControllers/SettingsViewController.cpp
+++ src/UI/ViewControllers/SettingsViewController.cpp
@@ -3,12 +3,156 @@
 #include "assets.hpp"
 
 #include "bsml/shared/BSML.hpp"
+#include "bsml/shared/BSML/Components/Settings/SliderSetting.hpp"
+
+#include "UnityEngine/Color.hpp"
+#include "UnityEngine/UI/Button.hpp"
+
+#define DEFINE_BSML_PROPERTY(type, name, get, set)          \
+type SettingsViewController::get_##name() get               \
+void SettingsViewController::set_##name(type value) set
 
 DEFINE_TYPE(RumbleMod::UI::ViewControllers, SettingsViewController);
+
+using namespace BSML;
+using namespace UnityEngine;
+using namespace UnityEngine::UI;
 
 namespace RumbleMod::UI::ViewControllers {
     void SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
         if (!firstActivation) return;
         BSML::parse_and_construct(::IncludedAssets::SettingsView_bsml, get_transform(), this);
     }
+
+    DEFINE_BSML_PROPERTY(bool, noteEnabled,
+        { // get
+            return getModConfig().noteEnabled.GetValue();
+        },
+        { // set
+            getModConfig().noteEnabled.SetValue(value);
+            noteEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            noteInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            noteInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, noteStrength,
+        { // get
+            return getModConfig().noteStrength.GetValue();
+        },
+        { // set
+            getModConfig().noteStrength.SetValue(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(bool, chainEnabled,
+        { // get
+            return getModConfig().chainEnabled.GetValue();
+        },
+        { // set
+            getModConfig().chainEnabled.SetValue(value);
+            chainEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            chainInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            chainInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, chainStrength,
+        { // get
+            return getModConfig().chainStrength.GetValue();
+        },
+        { // set
+            getModConfig().chainStrength.SetValue(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(bool, arcEnabled,
+        { // get
+            return getModConfig().arcEnabled.GetValue();
+        },
+        { // set
+            getModConfig().arcEnabled.SetValue(value);
+            arcEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            arcInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            arcInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, arcStrength,
+        { // get
+            return getModConfig().arcStrength.GetValue();
+        },
+        { // set
+            getModConfig().arcStrength.SetValue(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(bool, saberEnabled,
+        { // get
+            return getModConfig().saberEnabled.GetValue();
+        },
+        { // set
+            getModConfig().saberEnabled.SetValue(value);
+            saberEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            saberInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            saberInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, saberStrength,
+        { // get
+            return getModConfig().saberStrength.GetValue();
+        },
+        { // set
+            getModConfig().saberStrength.SetValue(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(bool, wallEnabled,
+        { // get
+            return getModConfig().wallEnabled.GetValue();
+        },
+        { // set
+            getModConfig().wallEnabled.SetValue(value);
+            wallEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            wallInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            wallInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, wallStrength,
+        { // get
+            return getModConfig().wallStrength.GetValue();
+        },
+        { // set
+            getModConfig().wallStrength.SetValue(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(bool, uiEnabled,
+        { // get
+            return getModConfig().uiEnabled.GetValue();
+        },
+        { // set
+            getModConfig().uiEnabled.SetValue(value);
+            uiEnabledText->set_color(value ? Color::get_green() : Color::get_red());
+
+            uiInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
+            uiInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+        }
+    )
+
+    DEFINE_BSML_PROPERTY(float, uiStrength,
+        { // get
+            return getModConfig().uiStrength.GetValue();
+        },
+        { // set
+            getModConfig().uiStrength.SetValue(value);
+        }
+    )
 }
