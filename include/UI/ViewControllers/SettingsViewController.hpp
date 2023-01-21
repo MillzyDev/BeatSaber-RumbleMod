5f06da2c3942da0e149b215e106d4d2dafd1476c
Millzy <millzyyt08@gmail.com>
2023-01-21 14:06:05 +0000

    more fields, and rumble tests

--- include/UI/ViewControllers/SettingsViewController.hpp
+++ include/UI/ViewControllers/SettingsViewController.hpp
@@ -3,31 +3,56 @@
 #include "custom-types/shared/macros.hpp"
 #include "bsml/shared/macros.hpp"
 
-#include "HMUI/ViewController.hpp"
-#include "TMPro/TextMeshProUGUI.hpp"
-#include "UnityEngine/UI/HorizontalLayoutGroup.hpp"
+#include "bsml/shared/BSML/Components/Settings/SliderSetting.hpp"
 
+#include "HMUI/ViewController.hpp"
+#include "Libraries/HM/HMLib/VR/HapticPresetSO.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+#include "UnityEngine/UI/Button.hpp"
+
+#include "GlobalNamespace/HapticFeedbackController.hpp"
+
+#define HapticFeedbackController GlobalNamespace::HapticFeedbackController
+#define HapticPresetSO Libraries::HM::HMLib::VR::HapticPresetSO
 #define TextMeshProUGUI TMPro::TextMeshProUGUI
-#define HorizontalLayoutGroup UnityEngine::UI::HorizontalLayoutGroup
+#define SliderSetting BSML::SliderSetting
+#define Button UnityEngine::UI::Button
 
 #define GET_METHOD(method) il2cpp_utils::il2cpp_type_check::MetadataGetter<&method>::get()
 
 DECLARE_CLASS_CODEGEN(RumbleMod::UI::ViewControllers, SettingsViewController,::HMUI::ViewController,
+    DECLARE_INSTANCE_FIELD(HapticPresetSO *, hapticPresetSO);
+    DECLARE_INSTANCE_FIELD(HapticFeedbackController *, hapticFeedbackController);
+
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, noteEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, noteInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, noteSlider);
+    DECLARE_INSTANCE_FIELD(Button *, noteButton);
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, chainEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, chainInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, chainSlider);
+    DECLARE_INSTANCE_FIELD(Button *, chainButton);
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, arcEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, arcInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, arcSlider);
+    DECLARE_INSTANCE_FIELD(Button *, arcButton);
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, saberEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, saberInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, saberSlider);
+    DECLARE_INSTANCE_FIELD(Button *, saberButton);
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, wallEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, wallInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, wallSlider);
+    DECLARE_INSTANCE_FIELD(Button *, wallButton);
     DECLARE_INSTANCE_FIELD(TextMeshProUGUI *, uiEnabledText);
-    DECLARE_INSTANCE_FIELD(HorizontalLayoutGroup *, uiInteractables);
+    DECLARE_INSTANCE_FIELD(SliderSetting *, uiSlider);
+    DECLARE_INSTANCE_FIELD(Button *, uiButton);
 
     DECLARE_OVERRIDE_METHOD(void, DidActivate, GET_METHOD(::HMUI::ViewController::DidActivate),
                             bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+
+    DECLARE_INSTANCE_METHOD(void, TestRumble, float strength, float duration);
+    DECLARE_INSTANCE_METHOD(void, TestNoteRumble);
+    DECLARE_INSTANCE_METHOD(void, TestChainRumble);
+    DECLARE_INSTANCE_METHOD(void, TestArcRumble);
+    DECLARE_INSTANCE_METHOD(void, TestSaberRumble);
+    DECLARE_INSTANCE_METHOD(void, TestWallRumble);
+    DECLARE_INSTANCE_METHOD(void, TestUIRumble);
 
     DECLARE_BSML_PROPERTY(bool, noteEnabled);
     DECLARE_BSML_PROPERTY(float, noteStrength);
@@ -43,5 +68,8 @@ DECLARE_CLASS_CODEGEN(RumbleMod::UI::ViewControllers, SettingsViewController,::H
     DECLARE_BSML_PROPERTY(float, uiStrength);
 )
 
+#undef HapticFeedbackController
+#undef HapticPresetSO
 #undef TextMeshProUGUI
-#undef HorizontalLayoutGroup
+#undef SliderSetting
+#undef Button
--- src/UI/ViewControllers/SettingsViewController.cpp
+++ src/UI/ViewControllers/SettingsViewController.cpp
@@ -1,32 +1,82 @@
 #include "UI/ViewControllers/SettingsViewController.hpp"
 #include "Configuration/ModConfig.hpp"
 #include "assets.hpp"
+#include "logging.hpp"
 
 #include "bsml/shared/BSML.hpp"
-#include "bsml/shared/BSML/Components/Settings/SliderSetting.hpp"
 
 #include "UnityEngine/Color.hpp"
-#include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/Resources.hpp"
+#include "UnityEngine/ScriptableObject.hpp"
+#include "UnityEngine/XR/XRNode.hpp"
+#include "VRUIControls/VRInputModule.hpp"
 
 #define DEFINE_BSML_PROPERTY(type, name, get, set)          \
 type SettingsViewController::get_##name() get               \
 void SettingsViewController::set_##name(type value) set
 
 #define CHANGED_RUMBLE_VALUE_ENABLED(value, text, slider, button) \
-text->set_color(value ? Color::get_green() : Color::get_red();      \
-slider->set_interactable(value);                                    \
+text->set_color((value) ? Color::get_green() : Color::get_red()); \
+slider->set_interactable(value);                                  \
 button->set_interactable(value);
 
 DEFINE_TYPE(RumbleMod::UI::ViewControllers, SettingsViewController);
 
 using namespace BSML;
+using namespace Libraries::HM::HMLib::VR;
 using namespace UnityEngine;
 using namespace UnityEngine::UI;
+using namespace UnityEngine::XR;
+using namespace VRUIControls;
 
 namespace RumbleMod::UI::ViewControllers {
     void SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
         if (!firstActivation) return;
         BSML::parse_and_construct(::IncludedAssets::SettingsView_bsml, get_transform(), this);
+        hapticPresetSO = ScriptableObject::CreateInstance<HapticPresetSO *>();
+    }
+
+    void SettingsViewController::TestRumble(float strength, float duration) {
+        if (!hapticFeedbackController) {
+            ArrayW<VRInputModule *> vrInputModules = Resources::FindObjectsOfTypeAll<VRInputModule *>();
+            VRInputModule *vrInputModule = vrInputModules.FirstOrDefault();
+            if (vrInputModule)
+                hapticFeedbackController = vrInputModule->hapticFeedbackController;
+        }
+
+        if (hapticFeedbackController) {
+            hapticPresetSO->strength = strength;
+            hapticPresetSO->duration = duration;
+            hapticFeedbackController->PlayHapticFeedback(XRNode::LeftHand, hapticPresetSO);
+            hapticFeedbackController->PlayHapticFeedback(XRNode::RightHand, hapticPresetSO);
+        }
+        else {
+            ERROR("Error getting HapticFeedbackController");
+        }
+    }
+
+    void SettingsViewController::TestNoteRumble() {
+        TestRumble(getModConfig().noteStrength.GetValue(), 1.0f);
+    }
+
+    void SettingsViewController::TestChainRumble() {
+        TestRumble(getModConfig().chainStrength.GetValue(), 0.2f);
+    }
+
+    void SettingsViewController::TestArcRumble() {
+        TestRumble(getModConfig().arcStrength.GetValue(), 1.0f);
+    }
+
+    void SettingsViewController::TestSaberRumble() {
+        TestRumble(getModConfig().saberStrength.GetValue(), 1.0f);
+    }
+
+    void SettingsViewController::TestWallRumble() {
+        TestRumble(getModConfig().wallStrength.GetValue(), 1.0f);
+    }
+
+    void SettingsViewController::TestUIRumble() {
+        TestRumble(getModConfig().uiStrength.GetValue(), 0.02f);
     }
 
     DEFINE_BSML_PROPERTY(bool, noteEnabled,
@@ -35,10 +85,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().noteEnabled.SetValue(value);
-            noteEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            noteInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            noteInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, noteEnabledText, noteSlider, noteButton)
         }
     )
 
@@ -57,10 +104,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().chainEnabled.SetValue(value);
-            chainEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            chainInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            chainInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, chainEnabledText, chainSlider, chainButton)
         }
     )
 
@@ -79,10 +123,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().arcEnabled.SetValue(value);
-            arcEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            arcInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            arcInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, arcEnabledText, arcSlider, arcButton)
         }
     )
 
@@ -101,10 +142,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().saberEnabled.SetValue(value);
-            saberEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            saberInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            saberInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, saberEnabledText, saberSlider, saberButton)
         }
     )
 
@@ -123,10 +161,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().wallEnabled.SetValue(value);
-            wallEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            wallInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            wallInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, wallEnabledText, wallSlider, wallButton)
         }
     )
 
@@ -145,10 +180,7 @@ namespace RumbleMod::UI::ViewControllers {
         },
         { // set
             getModConfig().uiEnabled.SetValue(value);
-            uiEnabledText->set_color(value ? Color::get_green() : Color::get_red());
-
-            uiInteractables->GetComponentInChildren<SliderSetting *>()->set_interactable(value);
-            uiInteractables->GetComponentInChildren<Button *>()->set_interactable(value);
+            CHANGED_RUMBLE_VALUE_ENABLED(value, uiEnabledText, uiSlider, uiButton)
         }
     )
 
